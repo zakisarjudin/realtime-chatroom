79060a3d8f8a821d2bb50323ff54d144a25d8998
zakisarjudin <zakisarjudin@gmail.com>
2023-03-07 22:00:14 +0700

    update config file

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,12 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDg6JyOqGBSIRFsp4rPUcDH9Ssg8r6llOs",
-  authDomain: "fir-chatroom-b26b9.firebaseapp.com",
-  projectId: "fir-chatroom-b26b9",
-  storageBucket: "fir-chatroom-b26b9.appspot.com",
-  messagingSenderId: "915181393353",
-  appId: "1:915181393353:web:f368f33242d7eace010418",
+  //set ur own config here
 };
 
 // Initialize Firebase
